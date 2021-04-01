1d6502723acbb90c496347d65b1dfd9ebfc418c7
emmafinkel <59944135+emmafinkel@users.noreply.github.com>
2021-04-01 14:48:07 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,67 @@
+var video;
+video = document.getElementById("player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	//video = document.querySelector(".video");
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = (video.volume * 100) + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.95;
+	console.log("Speed: " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function(){
+	video.playbackRate = video.playbackRate / 0.95;
+	console.log("Speed: " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 15 >= video.duration) {
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime += 15;
+	}
+	console.log(video.currentTime);
+	video.play();
+});
+
+document.querySelector("#mute").addEventListener("click", function(){
+	if (video.muted == false) {
+		video.muted = true;
+		console.log("muted");
+		document.querySelector("#mute").innerHTML = "unmute";
+	}
+	else {
+		video.muted = false;
+		console.log("unmuted");
+		document.querySelector("#mute").innerHTML = "mute";
+	}
+});
+
+document.querySelector("#slider").addEventListener("click", function(){
+	video.volume = (document.querySelector("#slider").value) / 100;
+	console.log(video.volume);
+});
+
+document.querySelector("#vintage").addEventListener("click", function(){
+	video.classList.add("oldSchool");
+	console.log("Old School");
+});
+
+document.querySelector("#orig").addEventListener("click", function(){
+	video.classList.remove("oldSchool");
+	console.log("Original");
+});
