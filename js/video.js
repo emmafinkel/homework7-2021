62c248f9e2a05928a7eb58b7956e892b46da5a02
emmafinkel <59944135+emmafinkel@users.noreply.github.com>
2021-04-01 15:05:12 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -53,7 +53,7 @@ document.querySelector("#mute").addEventListener("click", function(){
 
 document.querySelector("#slider").addEventListener("click", function(){
 	video.volume = (document.querySelector("#slider").value) / 100;
-	document.querySelector("#slider").innerHTML = (video.volume * 100) + "%";
+	document.querySelector("#volume").innerHTML = (video.volume * 100) + "%";
 	console.log(video.volume);
 });
 
